ad11d4c22d50af8e4226fb440d536f7f6b36594d
mariannematias <mariannec.matias@gmail.com>
2019-05-08 10:57:59 +0800

    added index-2 page

--- public/js/custom.js
+++ public/js/custom.js
@@ -49,4 +49,16 @@ $(document).ready(function(){
         $(".monthly-section").hide();
         $("#div"+btnId).fadeIn(500);;
     });
+
+    /* fade in navbar */
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 400) {
+				$('#navbar-top').fadeIn();
+			} else {
+				$('#navbar-top').fadeOut();
+			}
+		});
+	});
+
 });
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,33 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const home = require('../pages/home.json')
 const global = require('../pages/global.json')
+const blog = require('../pages/blog.json')
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
 	information, view here: https://mustache.github.io/#demo */
-	router.get('/', (req, res) => {
-		const data = {
-			page: home,
-			global: global
-		}
-		res.render('index', data)
-	})
+router.get('/', (req, res) => {
+	const data = {
+		page: home,
+		global: global
+	}
+	res.render('index', data)
+})
+
+router.get('/index-2', (req, res) => {
+	const data = {
+		page: home,
+		global: global
+	}
+	res.render('index-2', data)
+})
+
+router.get('/blog', (req, res) => {
+	const data = {
+		page: blog,
+		global: global
+	}
+	res.render('blog', data)
+})
 
 module.exports = router
