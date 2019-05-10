34cc197aaad232a737598b5fafc2e411ab9f42f8
mariannematias <mariannec.matias@gmail.com>
2019-05-10 10:02:44 +0800

    single blog updates

---
+++ public/js/slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding: "0px",
+        speed: 500,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+});
