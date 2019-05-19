b7cb03c47b13a4906a39b4f0000d9621cece668c
mariannematias <mariannec.matias@gmail.com>
2019-05-20 06:47:33 +0800

    layout updates

--- public/js/custom.js
+++ public/js/custom.js
@@ -38,4 +38,9 @@ $(document).ready(function(){
     };
 
     typingEffect();
+
+    $("a.register-btn").click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1200);
+    });
 });
